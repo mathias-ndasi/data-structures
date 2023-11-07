8577b35943c165805b6494a725cac6ba69595d1d
Mathias Ndasi <mathiasndasi@gmail.com>
2023-11-07 08:37:26 +0100

    link list

---
+++ linked-list/index.js
@@ -0,0 +1,28 @@
+const { Node } = require("./node");
+
+class LinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    push(value) {
+        const newNode = new Node(value);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.length++;
+
+        return this;
+    }
+}
+
+module.exports = LinkedList;
---
+++ main.js
@@ -0,0 +1,5 @@
+const LinkedList = require("./linked-list");
+
+let myLinkedList = new LinkedList(4);
+myLinkedList.push(7);
+console.log(myLinkedList)
